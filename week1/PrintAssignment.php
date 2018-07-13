a49d794ec4ff2d559b08b3668fa90e855f99c70e
jhaastrup <jhaastrup21@gmail.com>
2018-07-13 15:12:20 +0100

    Add files via upload
    This focus was on printing out in php.

---
+++ week1/PrintAssignment.php
@@ -0,0 +1,33 @@
+<html>
+<head><title>print assignment</title></head>
+
+<body>
+<?php  
+
+//phrase1 holds the the text this is nigeria.
+$phrase1 = "This is Nigeria by falz"  ;
+
+//phrase2 holds the text actually shows some reality.
+$phrase2 = "Actually shows some reality";
+
+
+print $phrase1 . " " . $phrase2; 
+
+echo "<br/>";
+
+echo $phrase1 ." " . $phrase2;
+
+
+
+
+
+?>
+
+
+
+
+</body>
+
+
+
+</html>
